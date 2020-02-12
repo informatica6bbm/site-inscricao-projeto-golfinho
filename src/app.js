1673920976a231920a4938ff1ad9e28fab54774c
Vinicius Lira <viniciusklira@gmail.com>
2020-02-12 18:02:18 -0300

    form avaliação

--- src/app.js
+++ src/app.js
@@ -15,10 +15,12 @@ app.set('view engine', 'html');
 app.use(express.static('./public'));
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     next();
  });
 
