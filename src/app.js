c7a80fbbf26fdc5b4f9bc44b6e9bd2ea06621e16
Vinicius Lira <viniciusklira@gmail.com>
2020-02-13 15:11:45 -0300

    fotos

--- src/app.js
+++ src/app.js
@@ -12,16 +12,11 @@ app.set('view engine', 'html');
 app.use(express.static('./public'));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://projetogolfinho6bbm.heroku.com/pessoa/avaliar");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Custom-Header');
-    res.setHeader("Content-Security-Policy", "default-src 'self'");
-    // res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-    // res.setHeader('Access-Control-Allow-Credentials', true);
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
