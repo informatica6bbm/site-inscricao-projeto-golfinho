4ef04448142788d72f59b72ba7a1cf321040d9a4
informatica6bbm <informatica6bbm@gmail.com>
2021-01-27 14:19:44 -0300

    teste

--- src/routes/pessoa-routes.js
+++ src/routes/pessoa-routes.js
@@ -6,10 +6,12 @@ const controller = require('./../controllers/pessoa-controller');
 
 router.get('/vagas', controller.get);
 
-//router.get('/cidades', controller.get);
-
-//router.get('/clubesCidade/:id', controller.get);
-
 router.post('/avaliar', controller.avaliar);
 
+router.get('/cidades', controller.get);
+
+router.get('/clubesCidade/:id', controller.get);
+
 router.post('/', controller.post);
+
+module.exports = router;
